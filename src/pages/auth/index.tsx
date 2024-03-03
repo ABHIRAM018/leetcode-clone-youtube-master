fedc9d5226f45b00cad35fc35b1c3f6cd9ece234
ABHIRAM <133672887+ABHIRAM018@users.noreply.github.com>
2024-03-03 13:38:22 +0530

    Add files via upload

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,4 +1,5 @@
 import { authModalState } from "@/atoms/authModalAtom";
+
 import AuthModal from "@/components/Modals/AuthModal";
 import Navbar from "@/components/Navbar/Navbar";
 import { useAuthState } from "react-firebase-hooks/auth";
@@ -7,6 +8,7 @@ import { useRecoilValue } from "recoil";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import Image from "next/image";
+
 type AuthPageProps = {};
 
 const AuthPage: React.FC<AuthPageProps> = () => {
@@ -26,7 +28,7 @@ const AuthPage: React.FC<AuthPageProps> = () => {
 		<div className='bg-gradient-to-b from-gray-600 to-black h-screen relative'>
 			<div className='max-w-7xl mx-auto'>
 				<Navbar />
-				<div className='flex items-center justify-center h-[calc(100vh-5rem)] pointer-events-none select-none'>
+				<div className='flex items-center justify-center h-[calc(100vh - 5rem)] pointer-events-none select-none'>
 					<Image src='/hero.png' alt='Hero img' width={700} height={700} />
 				</div>
 				{authModal.isOpen && <AuthModal />}
@@ -34,4 +36,5 @@ const AuthPage: React.FC<AuthPageProps> = () => {
 		</div>
 	);
 };
+
 export default AuthPage;
